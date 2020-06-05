741fca613246683495e3fab2606f133dfd9f1a52
Michael Knoll <mknoll@fearless.tech>
2020-06-04 22:06:49 -0400

    Updates

---
+++ precache-manifest.2c7371f23476b0d2593446542f62e583.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a222225e6da344e89d757631b42df75",
+    "url": "/NotNFL/index.html"
+  },
+  {
+    "revision": "dc976b92c5bdd02941fa",
+    "url": "/NotNFL/static/css/main.5de1958c.chunk.css"
+  },
+  {
+    "revision": "3cb0d777fb8b5c5c87f7",
+    "url": "/NotNFL/static/js/2.86d3fe4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NotNFL/static/js/2.86d3fe4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc976b92c5bdd02941fa",
+    "url": "/NotNFL/static/js/main.c55bb448.chunk.js"
+  },
+  {
+    "revision": "970db6fcd13224f857a6",
+    "url": "/NotNFL/static/js/runtime-main.9d5d65ae.js"
+  },
+  {
+    "revision": "9b1b5ea43ec8ba5b5dde263198504788",
+    "url": "/NotNFL/static/media/logo.9b1b5ea4.svg"
+  }
+]);
